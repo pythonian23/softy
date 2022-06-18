3ff0e49157ee66054b649480ee3dd4207621535b
pythonaian23 <pythonaian23@gmail.com>
2022-06-18 18:03:54 +0900

    some documentation

--- softy/node.py
+++ softy/node.py
@@ -3,16 +3,22 @@ from .link import Link
 
 
 class Node:
-    def __init__(self, mass, loc=(0, 0)):
+    def __init__(self, mass, loc=(0, 0), unique_id=None):
+        """
+        If unique_id is provided, it is best to provide it for every node in an object to ensure safety.
+        """
         self.loc = np.array(loc, dtype="float32")
         self.mass = mass
         self.velocity = np.zeros(2, dtype="float32")
         self._force_sum = np.zeros(2, dtype="float32")
-        
+
         self._prev: Link = None
         self._next: Link = None
 
     def connect_forward(self, node):
+        """
+        Connect another node as the next node. This connects both ways.
+        """
         link = Link(self, node)
         self._next = link
         node._prev = link
