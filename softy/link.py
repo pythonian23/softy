bfbee0c3f2c12fdaf7d04c003daba3df51708d85
pythonaian23 <pythonaian23@gmail.com>
2022-06-24 01:55:07 +0900

    link improvements

--- softy/link.py
+++ softy/link.py
@@ -1,5 +1,15 @@
+from .node import Node
+
+import numpy as np
+
+
 class Link:
-    def __init__(self, start, end, spring=0):
+    def __init__(self, start: Node, end: Node, spring=0):
         self.start = start
         self.end = end
         self.spring = spring
+        self._neutral_length = np.linalg.norm(start.loc - end.loc)
+
+    def apply_force(self, force: np.ndarray):
+        self.start.apply_force(force)
+        self.end.apply_force(force)
